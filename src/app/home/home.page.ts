efd53074c2d90ca0166e2427255a959e125b3c9d
mohamed <mohamedh@web-3.in>
2023-04-19 16:11:06 +0530

    intial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule, FormsModule]
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  @ViewChild("header") header: HTMLElement;
+  details = ['welcome', 'jack', 'joe doe', 'welcome', 'jack', 'joe doe', 'welcome', 'jack', 'joe doe', 'welcome', 'jack', 'joe doe', 'welcome', 'jack', 'joe doe', 'welcome', 'jack', 'joe doe', 'welcome', 'jack', 'joe doe', 'welcome', 'jack', 'joe doe', 'welcome', 'jack', 'joe doe', 'welcome', 'jack', 'joe doe', 'welcome', 'jack', 'joe doe', 'welcome', 'jack', 'joe doe'];
+
+  constructor(public element: ElementRef,
+    public renderer: Renderer2) { }
+
+  ngOnInit() {
+  }
+  ionViewWillEnter() {
+    this.renderer.setStyle(this.header['el'], 'webkitTransition', 'top 700ms');
+  }
+
+  onContentScroll(event: any) {
+    if (event.detail.scrollTop >= 50) {
+      this.renderer.setStyle(this.header['el'], 'top', '-76px');
+    } else {
+      this.renderer.setStyle(this.header['el'], 'top', '0px');
+    }
+  }
+
 }
